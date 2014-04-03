56e5bc6c42a9f9a9729ed9511ee4ad53ccf41316
John Schulz <jschulz@twitter.com>
2014-04-02 20:41:04 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+(function() {
+    !function() {
+        var d3 = {
+            version: "3.4.4"
+        };
+        d3.extent = function(array, f) {
+            var i = -1, n = array.length, a, b, c;
+            if (arguments.length === 1) {
+                while (++i < n && !((a = c = array[i]) != null && a <= a)) a = c = undefined;
+                while (++i < n) if ((b = array[i]) != null) {
+                    if (a > b) a = b;
+                    if (c < b) c = b;
+                }
+            } else {
+                while (++i < n && !((a = c = f.call(array, array[i], i)) != null && a <= a)) a = undefined;
+                while (++i < n) if ((b = f.call(array, array[i], i)) != null) {
+                    if (a > b) a = b;
+                    if (c < b) c = b;
+                }
+            }
+            return [ a, c ];
+        };
+        if (typeof define === "function" && define.amd) {
+            define(d3);
+        } else if (typeof module === "object" && module.exports) {
+            module.exports = d3;
+        } else {
+            this.d3 = d3;
+        }
+    }();
+})();
